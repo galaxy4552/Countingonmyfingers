f27ef87816fce4ca0f56bcd4e2951ad76c828b4c
galaxy4552 <galaxy4552@livemail.tw>
2025-09-28 01:27:45 +0800

    Update time3.py

--- time3.py
+++ time3.py
@@ -52,7 +52,6 @@ st.title("掐指一算")
 
 if st.button("開始科學預測"):
     from datetime import datetime
-     now = datetime.now()
     lunar = ZhDate.from_datetime(now)
     lunar_month = lunar.month
     lunar_day = lunar.day
